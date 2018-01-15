7a46f36372083e87360725790e7e6213ea757d90
Anderson Davanse <anderson.davanse@tmf-group.com>
2018-01-15 14:04:46 -0200

    general improvements

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -8,46 +8,71 @@ window.addEventListener('load', function (event) {
             return response.json();
         })
         .then(function (res) {
-
-            for(coinProperty in res.bpi){
+            console.log(res);
+            for (coinProperty in res.bpi) {
+                res.bpi[coinProperty]['updatedISO'] = res.time.updatedISO;
                 coins.push(res.bpi[coinProperty]);
             }
-            
+
         })
         .catch(function (err) {
             console.log(err);
         });
 
-        var bitcoinPrice = document.getElementById("bitcoinPrice");
+    document.getElementsByName('options').forEach(function (value, index, lst){
+        value.onclick = function(event) {
+            UpdatePrice(event.path[0]);
+        }
+    });
 
-        bitcoinPrice.addEventListener('keyup', function(event) {
-            console.log(event);
-            if (event.key in ['0','1','2','3','4','5','6','7','8','9','.',','] || event.key == 'Backspace') {
-                if (parseFloat(bitcoinPrice.value) !== NaN) {
-                    lastValue = parseFloat(bitcoinPrice.value);
+    var bitcoinPrice = document.getElementById('bitcoinPrice');
 
-                    for(var i = 0; i < coins.length; i++) {
-                       convertToCurrency(coins[i]);
-                    }
-                }
-            }        
-        })
+    bitcoinPrice.addEventListener('keyup', function (event) {
+        var currencyLbl = document.getElementsByClassName('is-checked')[0];
+        var ckBox = currencyLbl.children.namedItem('options');
+        console.log(ckBox.value);
+
+        if (event.key in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ','] || event.key == 'Backspace') {
+            if (parseFloat(bitcoinPrice.value) !== NaN) {
+                lastValue = parseFloat(bitcoinPrice.value);
+                UpdatePrice(ckBox);
+            }
+        }
+    })
 });
+
+function UpdatePrice(ckBox) {
+    for (var i = 0; i < coins.length; i++) {
+        if (ckBox.value === coins[i].code) {
+            convertToCurrency(coins[i]);
+        }
+    }
+}
 
 function convertToCurrency(coin) {
     var carts = document.getElementById('cards');
-    
+
     for (var i = 0; i < carts.childNodes.length; i++) {
-       carts.removeChild(carts.childNodes[i]);
+        carts.removeChild(carts.childNodes[i]);
     }
 
     var newCart = document.createElement('div');
-    newCart.innerHTML =
+    var html =
         '<div class="center-content">' +
         '     <h2>' + coin.code +
-        '         ' + parseFloat(coin.rate_float * lastValue).toFixed(2) +
-        '     </h2>' +
-        '  <span> (' + coin.description + ') </span>' +
-        '</div>';
-        carts.appendChild(newCart);    
+        '         ' + parseFloat(coin.rate_float * lastValue).toLocaleString() +
+        '     </h2>';
+
+    if (coin.description) {
+        html = html + '  <span> (' + coin.description + ') </span>';
+    }
+
+    if (coin.updatedISO) {
+        var updateDt = new Date(coin.updatedISO);
+        html = html + '  <br><span> Atualizado em ' + updateDt.toLocaleTimeString() + '</span>';
+    }
+
+    html = html + '</div>';
+    newCart.innerHTML = html;
+    carts.appendChild(newCart);
 }
