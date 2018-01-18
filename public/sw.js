21260656635529af3a48e4df852cd7df1cffbb62
Anderson Davanse <anderson.davanse@tmf-group.com>
2018-01-18 13:03:12 -0200

    dynamic cache added

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
 
-var STATIC_CACHE_NAME = "static-cache-v2";
+var STATIC_CACHE_NAME = "static-cache-v4";
+var DYNAMIC_CACHE_NAME = "dynamic-cache-v1";
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(STATIC_CACHE_NAME)
          .then(function(cache) {
              return cache.addAll([
-                 "/",
                 "manifest.json",
                 "index.html",
                 "src/css/app.css",
@@ -25,22 +25,36 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     console.log('Fetch event for ', event.request.url);
 
-    event.respondWith(
+    event.respondWith(        
+           
       caches.match(event.request).then(function(response) {
         if (response) {
+
           console.log('Found ', event.request.url, ' in cache');
           return response;
+
+        } else {
+
+          return caches.open(DYNAMIC_CACHE_NAME)
+              .then(function(cache) {
+                  
+                console.log(event.request.url,  " added to dynamic cache.");
+                  console.log('Network request for ', event.request.url);
+
+                  return fetch(event.request)
+                      .then(function(networkResponse) {
+
+                        if (event.request.url.indexOf('.json') == -1){
+                          cache.put(event.request, networkResponse.clone());
+                        }
+
+                        return networkResponse;  
+                      })
+              });
         }
 
-        console.log('Network request for ', event.request.url);
-        return fetch(event.request)
-  
-        // TODO 4 - Add fetched files to the cache
-  
       }).catch(function(error) {  
-        // TODO 6 - Respond with custom offline page
-        console.log(error);
-  
+        console.log(error);  
       })
-    );
+    )
   });
